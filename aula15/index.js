01e59f9bdac63dd6e1478ee760f154a15f28f84d
yribeiro333 <yribeiro333@gmail.com>
2026-03-08 19:48:12 -0300

    add initial implementation of mathematical operations examples in aula15/index.js

---
+++ aula15/index.js
@@ -0,0 +1,8 @@
+// console.log(100 / 0); // retorna Infinity, que é um valor especial do JavaScript que representa um valor infinito ou indeterminado
+
+// let num2 = Math.floor(num1); // arredonda para baixo, ou seja, para o número inteiro mais próximo que seja menor ou igual a num1
+// let num2 = Math.ceil(num1); // arredonda para cima, ou seja, para o número inteiro mais próximo que seja maior ou igual a num1
+// let num2 = Math.round(num1); // arredonda para o número mais próximo
+// console.log(Math.max(1,2,3,4,5, -10, -50, -1500, 9, 8, 7, 6)); // retorna o maior número entre os argumentos passados
+// console.log(Math.min(1,2,3,4,5, -10, -50, -1500, 9, 8, 7, 6)); // retorna o menor número entre os argumentos passados
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // retorna um número aleatório entre 5 (inclusive) e 10 (inclusive)
